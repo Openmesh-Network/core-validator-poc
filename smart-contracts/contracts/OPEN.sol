0ef78081d901b621f30b953e769ef4aebe219ddd
Plopmenz <plopmenz@gmail.com>
2023-11-22 13:41:22 +0100

    MIT license

--- smart-contracts/contracts/OPEN.sol
+++ smart-contracts/contracts/OPEN.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: None
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import {ERC20Votes, Votes, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
