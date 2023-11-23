604f6c076cc3e5c46b949ab765ecdcef54490cbb
Plopmenz <plopmenz@gmail.com>
2023-11-23 19:11:46 +0100

    OPEN contract sync

--- smart-contracts/contracts/OPEN.sol
+++ smart-contracts/contracts/OPEN.sol
@@ -1,9 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {ERC20Votes, Votes, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import {ERC20Votes, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import {IVotes} from "@openzeppelin/contracts/governance/utils/IVotes.sol";
+import {IERC5267} from "@openzeppelin/contracts/interfaces/IERC5267.sol";
+import {IERC6372} from "@openzeppelin/contracts/interfaces/IERC6372.sol";
 
 import {IERC20MintBurnable} from "./IERC20MintBurnable.sol";
 
@@ -21,6 +27,20 @@ contract OPEN is ERC20Votes, AccessControl, IERC20MintBurnable {
     ) ERC20(_name, _symbol) EIP712(_name, "1") {
         maxSupply = _maxSupply;
         _grantRole(DEFAULT_ADMIN_ROLE, _admin);
+    }
+
+    /// @inheritdoc AccessControl
+    function supportsInterface(
+        bytes4 _interfaceId
+    ) public view virtual override returns (bool) {
+        return
+            _interfaceId == type(IERC20).interfaceId ||
+            _interfaceId == type(IERC20Metadata).interfaceId ||
+            _interfaceId == type(IVotes).interfaceId ||
+            _interfaceId == type(IERC5267).interfaceId ||
+            _interfaceId == type(IERC6372).interfaceId ||
+            _interfaceId == type(IERC20MintBurnable).interfaceId ||
+            super.supportsInterface(_interfaceId);
     }
 
     /// @inheritdoc IERC20MintBurnable
